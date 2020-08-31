d50152a84f2e4de262380a1f0193622a58511b6e
Yukinobu Nagayasu <nagayasu@yukinobu.jp>
2020-08-31 20:41:16 +0900

    input/output 取り違えの修正

--- jpfold.py
+++ jpfold.py
@@ -7,10 +7,10 @@ from typing import TextIO
 
 
 def main(args: argparse.Namespace) -> int:
-    infile:  TextIO = sys.stdin  if args.input=='-' else open(args.input,'r',encoding='utf-8')
-    outfile: TextIO = sys.stdout if args.input=='-' else open(args.input,'w',encoding='utf-8')
+    infile:  TextIO = sys.stdin  if args.input=='-'  else open(args.input, 'r',encoding='utf-8')
+    outfile: TextIO = sys.stdout if args.output=='-' else open(args.output,'w',encoding='utf-8')
     for line in infile:
-        outfile.write(line.rstrip())
+        outfile.write(line)
     infile.close()
     outfile.close()
     return 0
@@ -18,8 +18,8 @@ def main(args: argparse.Namespace) -> int:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = '日本語整形ツール')
-    parser.add_argument('-w', '--width', type=int, default=66)
-    parser.add_argument('-i', '--input', type=str, default='-')
+    parser.add_argument('-w', '--width',  type=int, default=66)
+    parser.add_argument('-i', '--input',  type=str, default='-')
     parser.add_argument('-o', '--output', type=str, default='-')
     args = parser.parse_args()
     sys.exit( main(args) )
